116bb1532335ffc8f16d3146c867d3e7fa949ff5
Candace Ntembo <cjntembo@gmail.com>
2021-11-14 16:55:04 -0600

    tag and postreaction model

---
+++ models/post_reaction.py
@@ -0,0 +1,12 @@
+
+class Post_Reaction():
+    
+    def __init__(self, id, user_id, reaction_id, post_id):
+        self.id = id
+        self.user_id = user_id
+        self.reaction_id = reaction_id
+        self.post_id = post_id
+        self.user = None
+        self.reaction = None
+        self.post = None
+
