7b1a6976b40e5fe0b666fe7a99119b2b04d5294a
cod-y-clark <cody66clark@gmail.com>
2021-11-14 22:11:37 -0600

    Adding Post & Post_Tag classes

---
+++ models/post.py
@@ -0,0 +1,9 @@
+class Post():
+    def __init__(self, user_id, category_id, title, publication_date, image_url, content, approved):
+        self.id = id,
+        self.category_id = category_id,
+        self.title = title,
+        self.publication_date = publication_date,
+        self.image_url = image_url,
+        self.content = content,
+        self.approved = approved 
