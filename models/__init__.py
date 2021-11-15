ce241ed13aafeedf51fdab82a05bf3eda658a70a
cod-y-clark <cody66clark@gmail.com>
2021-11-14 22:31:25 -0600

    Creating classes and imports for Post and Post_Tag

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from .user import User
 from .demotion_queue import Demotion_Queue
 from .reaction import Reaction
 from .subscription import Subscription
+from .post import Post
+from .post_tag import Post_Tag
