db910c18be910d9e976999f10025807c1f0766d3
Candace Ntembo <cjntembo@gmail.com>
2021-11-15 18:49:36 -0600

    fixed model init

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from .user import User
 from .demotion_queue import Demotion_Queue
 from .reaction import Reaction
 from .subscription import Subscription
+from .post_reaction import Post_Reaction
+from .tag import Tag
