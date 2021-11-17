4a958855a72b1a6b6e4e522e8d6b5d6455eb790c
EugeneTerry <eugene.terry73@gmail.com>
2021-11-16 21:00:27 -0600

    fixed type-os in user and reaction models

--- models/reaction.py
+++ models/reaction.py
@@ -1,4 +1,4 @@
 class Reaction():
     def __init__(self, label, image_url):
         self.label = label
-        self.image_url - image_url
+        self.image_url = image_url
--- models/user.py
+++ models/user.py
@@ -1,13 +1,14 @@
+from datetime import datetime
 class User():
-  def __int__(self, id, first_name, last_name, email, bio, username, password, profile_image_url, created_on, active):
-    self.id = id,
-    self.first_name = first_name,
-    self.last_name = last_name,
-    self.email = email,
-    self.bio = bio,
-    self.username = username,
-    self.password = password,
-    self.profile_image_url = profile_image_url
-    self.created_on = created_on,
+  def __init__(self, first_name, last_name, email, bio='', password='', profile_image_url='', created_on=None, active=True, id=None):
+    self.id = id
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.bio = bio
+    self.username = email
+    self.password = password
+    self.profile_image_url = profile_image_url,
+    self.created_on = created_on or datetime.now()
     self.active = active
   
