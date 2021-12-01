9dd53032ea8cdd4a39d8ea8f26cc1be789b555ff
EugeneTerry <eugene.terry73@gmail.com>
2021-11-30 20:10:50 -0600

    fixed create post

--- posts/request.py
+++ posts/request.py
@@ -81,10 +81,10 @@ def create_post(new_post):
         
         db_cursor.execute("""
             INSERT INTO Posts (
-                id, user_id, category_id, title, publication_date, image_url, content, approved
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+                user_id, category_id, title, publication_date, image_url, content, approved
+            ) VALUES (?, ?, ?, ?, ?, ?, ?);
         """, (
-            new_post['id'], new_post['user_id'], new_post['category_id'], new_post['title'], new_post['publication_date'], new_post['image_url'], new_post['content'], new_post['approved'],
+            new_post['user_id'], new_post['category_id'], new_post['title'], new_post['publication_date'], new_post['image_url'], new_post['content'], new_post['approved'],
         ))
         
         id = db_cursor.lastrowid
